046f9c37ecfdd66ac506bb296fb3aeb56a0bdb0d
Anthony Barrera <barr.star77@gmail.com>
2024-12-07 20:41:11 -0600

    updated scripts

--- Scripts/deposit_area.gd
+++ Scripts/deposit_area.gd
@@ -3,6 +3,7 @@ extends Area3D
 var player : CharacterBody3D  # Reference to the player node
 @export var energy_add_amount = 5.0  # Amount to increase fire energy when a log is deposited
 var fire_slider : Slider  # Reference to the fire slider to update fuel
+var pickup_woodlog_script : Node
 
 func _ready():
 	add_to_group("deposit_area")
@@ -10,15 +11,29 @@ func _ready():
 	connect("body_entered", Callable(self, "_on_DepositArea_body_entered"))
 
 # This method will be called when a body enters the area
-func _on_DepositArea_body_entered(body):
+func _on_DepositArea_body_entered(body: Node):
 	if body.is_in_group("player"):  # Ensure it is the player
-		player = body  # Store a reference to the player
-		print("Player entered deposit area")
+		player = body
+		pickup_woodlog_script = player.get_node("pickup_woodlog")
+		if pickup_woodlog_script:
+			pickup_woodlog_script.on_deposit_area_entered = true
+		 # Store a reference to the player
+			print("Player entered deposit area")
+		else:
+			print("error unable to find pickup_woodlog_script")
+			
+func _on_body_exited(body: Node):
+	if body.is_in_group("player"):
+		if pickup_woodlog_script:
+			pickup_woodlog_script.on_deposit_area_entered = false
+			print("player exited deposit area")
 
 # This function will be called in _process when the player is close to the area and presses interact
 func _process(_delta):
-	if player and player.has_log:  # Ensure the player has a log and is close
-		if Input.is_action_just_pressed("interact"):  # Check if the interact button is pressed
+	if player and player.collected_logs > 0:  # Ensure the player has a log and is close
+		if fire_slider:
+			fire_slider.value += energy_add_amount
+			
 			deposit_log_into_fire()  # Deposit the log into the fire
 
 # This function adds the log to the fire slider
--- Scripts/raycast.gd
+++ Scripts/raycast.gd
@@ -3,42 +3,43 @@ extends RayCast3D
 var int_text : Control  # Reference to the UI for showing interact text
 var interactable : Node = null  # Reference to the interactable object (log or campfire)
 var player : Node3D  # Reference to the player character
-var max_interact_distance : float = 5.0  # Max distance at which the player can interact
+var max_interact_distance : float = 5.0 # Max distance at which the player can interact
 
 func _ready():
 	# Initialize the UI interact text node
 	int_text = get_node("/root/" + get_tree().current_scene.name + "/UI/interact text")
 	# Get the player node reference
-	player = get_node("/root/level/player")  # Adjust path if needed
+	player = get_node("/root/level/Player")  # Adjust path if needed
 
 # Called every frame to check if the raycast is colliding with an object
 func _process(_delta):
 	# Check if the raycast is colliding with something
 	if is_colliding():
 		var hit = get_collider()  # Get the collider that the raycast hit
-		# Check if the collider is a log object (part of the "pickup_log" group)
-		if hit and hit.is_in_group("pickup_log"):
+
+		# Check for log interaction
+		if hit.is_in_group("pickup_log"):
 			int_text.visible = true  # Show interact text
-			interactable = hit  # Set the interactable object to the log that was hit
+			interactable = hit
 			if Input.is_action_just_pressed("interact"):
-				# If the interact button is pressed, call interact_with_log() on the log
-				interactable.interact_with_log()  # Call the method on the log
-				interactable = null  # Clear the interactable after interacting
-		# Check if the collider is the campfire (part of the "campfire" group)
+				interactable.interact_with_log()  # Call log interaction
+				
+				if player != null:
+					player.collected_logs += 1
+					print("logs collected! Total logs: ", player.collected_logs)
+				interactable = null
+				
+
+		# Check for campfire interaction
 		elif hit.is_in_group("campfire"):
 			int_text.visible = true  # Show interact text
-			interactable = hit  # Set the interactable object to the campfire
-			# Check if the player is close enough to interact with the campfire
-			if player.position.distance_to(hit.position) <= max_interact_distance:
-				# If the interact button is pressed, interact with the campfire (add fuel)
-				if Input.is_action_just_pressed("interact"):
-					interactable.add_fuel()  # Call the method on the campfire
-					print("Interacted with the campfire.")
-					interactable = null  # Clear the interactable after interacting
-			else:
-				# Display message if the player is too far to interact
-				int_text.text = "You are too far from the campfire!"
+			interactable = hit
+			# If the interact button is pressed, add fuel to the fire
+			if Input.is_action_just_pressed("interact"):
+				interactable.add_fuel()  # Add fuel to the fire
+				print("Interacted with the campfire.")
+				interactable = null
 		else:
-			int_text.visible = false  # Hide the interact text if not colliding with a log or campfire
+			int_text.visible = false  # Hide interact text for other objects
 	else:
-		int_text.visible = false  # Hide the interact text if no collision is detected
+		int_text.visible = false  # Hide interact text if no collision
